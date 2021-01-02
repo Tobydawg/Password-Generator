1abe2c647e6c49d1e21f96c3028492b9a1aa2b77
Toby <agentalliancela33@gmail.com>
2021-01-01 22:57:02 -0800

    else if

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // Assignment code here
-
-let length = number(prompt("how many characters would you like your password to be? you can choose between 8 and 128"));
+let length = prompt("how many characters would you like your password to be? you can choose between 8 and 128 (use a comma and a space after each word) WARNING: some combinations will not work!");
 let charType = prompt("Choose character type: special, numeric, uppercase, lowercase");
 
 
@@ -11,7 +10,10 @@ function generatePassword(){
     var charSet = "";
     var charTypeLower =charType.toLowerCase();
 
-    if(charTypeLower ==="lowercase")
+
+    //single selection
+
+    if(charTypeLower ==="lowercase" )
     {
         charSet ="abcdefghijklmnopqrstuvwxyz";
     }
@@ -31,10 +33,147 @@ function generatePassword(){
         charSet ="!@#$%^&*()_+-=";
     }
 
-    //return value
+
+    //double selction combo
+
+    else if(charTypeLower ==="lowercase, " + "numeric" )
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz1234567890";
+    }
+
+    else if(charTypeLower ==="numeric, " + "lowercase" )
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz1234567890";
+    }
+
+    else if(charTypeLower ==="lowercase, " + "uppercase" )
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    else if(charTypeLower ==="uppercase, " + "lowercase" )
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    else if(charTypeLower ==="lowercase, " + "special" )
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=";
+    }
+
+    else if(charTypeLower ==="special, " + "lowercase" )
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=";
+    }
+
+    else if(charTypeLower ==="special, " + "numeric" )
+    {
+        charSet ="1234567890!@#$%^&*()_+-=";
+    }
+
+    else if(charTypeLower ==="numeric, " + "special" )
+    {
+        charSet ="1234567890!@#$%^&*()_+-=";
+    }
+
+    else if(charTypeLower ==="uppercase, " + "special" )
+    {
+        charSet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=";
+    }
+
+    else if(charTypeLower ==="special, " + "uppercase" )
+    {
+        charSet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=";
+    }
+
+    //triple selection combo
+
+    else if(charTypeLower ==="lowercase, " + "special, " + "uppercase" )
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    else if(charTypeLower ==="special, " + "lowercase, " + "uppercase" )
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    else if(charTypeLower ==="special, " + "uppercase, " + "lowercase" )
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    else if(charTypeLower ==="uppercase, " + "lowercase, " + "special" )
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    else if(charTypeLower ==="numeric, " + "lowercase, " + "uppercase" )
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    else if(charTypeLower ==="special, " + "numeric, " + "lowercase" )
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=1234567890";
+    }
+
+    else if(charTypeLower ==="uppercase, " + "numeric, " + "special" )
+    {
+        charSet ="!@#$%^&*()_+-=1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    else if(charTypeLower ==="numeric, " + "lowercase, " + "special" )
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=1234567890";
+    }
+
+    //quadruple selection combo
+
+    else if(charTypeLower ==="lowercase, " + "special, " + "uppercase, " + "numeric")
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    }
+    else if(charTypeLower ==="special, " + "uppercase, " + "numeric, " + "lowercase")
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    }
+    else if(charTypeLower ==="uppercase, " + "numeric, " + "lowercase, " + "special")
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    }
+    else if(charTypeLower ==="numeric, " + "lowercase, " + "special, " + "uppercase")
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    }
+    else if(charTypeLower ==="special, " + "lowercase, " + "uppercase, " + "numeric")
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    }
+    else if(charTypeLower ==="lowercase, " + "uppercase, " + "special, " + "numeric")
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    }
+    else if(charTypeLower ==="uppercase, " + "special, " + "numeric, " + "lowercase")
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    }
+    else if(charTypeLower ==="lowercase, " + "numeric, " + "uppercase, " + "special")
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    }
+
+    else if(charTypeLower ==="special, " + "numeric, " + "uppercase, " + "lowercase")
+    {
+        charSet ="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    }
+    
+    
+    
+
   var retVal = "";
   for (var i = 0; i < length; i++) {
-    //picks a character within charSet at index of random number
+    
     retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
   }
   return retVal;
