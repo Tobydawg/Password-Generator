c2b65d31d1671e3edb34cee59a0f57230ec80311
Toby <agentalliancela33@gmail.com>
2021-01-01 21:57:25 -0800

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // Assignment code here
+
 let length = number(prompt("how many characters would you like your password to be? you can choose between 8 and 128"));
 let charType = prompt("Choose character type: special, numeric, uppercase, lowercase");
 
@@ -6,32 +7,40 @@ let charType = prompt("Choose character type: special, numeric, uppercase, lower
  
 
 
-function getData(){
-    if(charType.toLowerCase ==="lowercase")
+function generatePassword(){
+    var charSet = "";
+    var charTypeLower =charType.toLowerCase();
+
+    if(charTypeLower ==="lowercase")
     {
         charSet ="abcdefghijklmnopqrstuvwxyz";
     }
 
-    else if(charType.toLowerCase ==="uppercase")
+    else if(charTypeLower ==="uppercase")
     {
         charSet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
 
-    else if(charType.toLowerCase ==="numeric")
+    else if(charTypeLower ==="numeric")
     {
         charSet ="1234567890";
     }
 
-    else if(charType.toLowerCase ==="special")
+    else if(charTypeLower ==="special")
     {
         charSet ="!@#$%^&*()_+-=";
     }
 
- 
-  
-  
+    //return value
+  var retVal = "";
+  for (var i = 0; i < length; i++) {
+    //picks a character within charSet at index of random number
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
   }
-      getData();
+  return retVal;
+}
+alert(generatePassword());
+
 
 // Get references to the #generate element
 
